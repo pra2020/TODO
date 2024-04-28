587ee60982d953c5ca765cef48cdf90a2c300c50
Pranav Kokate <pranavkokate14@gmail.com>
2024-04-28 22:03:06 +0530

    no modal btn

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -78,7 +78,7 @@ const Todos = () => {
         ))}
       </ul>
     </div>
-    <Button onClick={handleOpen}>Open modal</Button>
+    {/* <Button onClick={handleOpen}>Open modal</Button> */}
       <Modal
         open={open}
         onClose={handleClose}
